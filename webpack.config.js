27f9071bf9112c7badfccccbc6b17e006b77c2e6
Andrew F. Ly <Andrew_Ly@dell.com>
2017-12-11 14:44:02 -0600

    Add dimm package to cockpit

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,9 @@ var info = {
         "docker/console": [
             "docker/console.js",
         ],
+        "hardware/dimm": [
+            "hardware/main.js",
+        ],
 
         "kdump/kdump": [
             "kdump/kdump.js",
@@ -202,6 +205,9 @@ var info = {
         "docker/console.html",
         "docker/index.html",
         "docker/images/drive-harddisk-symbolic.svg",
+
+        "hardware/dimm.html",
+        "hardware/manifest.json",
 
         "kdump/index.html",
 
